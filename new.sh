6c28a987bb5eb01604374be7e235553eaff381e0
Lijo George <lijogrg444@gmail.com>
2019-06-24 15:06:50 +0100

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
+pwd=`dirname $0`
 sudo apt update
 sudo apt install nginx
 sudo apt install mysql-server
 sudo apt install php-fpm php-mysql
-sudo mkdir /var/www/
+read -e -p "Target directory: " -i "/var/www" dir
+dir=${dir:-"/var/www"}
+read -e -p "Database: " -i "wordpress" db
+db=${db:-"wordpress"}
+sudo apt-get install -y \
+	mysql-server \
+	nginx \
+	php-curl \
+	php-fpm \
+	php-gd \
+	php-mysql \
+	wget \
+	unzip
+  
